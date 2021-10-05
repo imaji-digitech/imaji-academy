68893e1a32186a4d46982edfca31089863671f8c
masif088 <mokhamadasif@gmail.com>
2021-10-05 08:32:28 +0700

    initial commit

---
+++ app/Http/Livewire/DashboardAdmin.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Feature;
+use App\Models\ImajiAcademy;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Livewire\Component;
+
+class DashboardAdmin extends Component
+{
+    public $data;
+    public $activity;
+    public $max;
+
+    public function mount()
+    {
+        $this->max=[];
+        $this->data['imajiAcademy'] = ImajiAcademy::get(['id'])->count();
+        $this->data['student'] = User::whereRole(3)->get(['id'])->count();
+        foreach (ImajiAcademy::get() as $im) {
+            $this->max[$im->id]=0;
+            $data = DB::select("
+SELECT
+  features.title,
+  COUNT(feature_activity_presences.id) as presence
+FROM feature_activity_presences JOIN feature_activities on feature_activities.id = feature_activity_presences.feature_activity_id
+    JOIN imaji_academy_features on imaji_academy_features.id = feature_activities.iaf_id
+    JOIN features on imaji_academy_features.feature_id = features.id
+WHERE imaji_academy_features.imaji_academy_id=$im->id
+AND feature_activity_presences.presence_status_id=1
+GROUP BY features.id,features.title,feature_activities.id;");
+            if ($data!=null) {
+                foreach ($data as $ac) {
+                    $this->activity[$im->id][$ac->title] = [];
+                }
+                foreach ($data as $ac) {
+                    array_push($this->activity[$im->id][$ac->title], $ac->presence);
+                }
+                foreach ($this->activity[$im->id] as $val) {
+                    if ($this->max[$im->id] < count($val)) {
+                        $this->max[$im->id] = count($val);
+                    }
+                }
+            }
+        }
+    }
+
+    public function render()
+    {
+
+        return view('livewire.dashboard-admin');
+    }
+}
---
+++ resources/views/livewire/dashboard-admin.blade.php
@@ -0,0 +1,124 @@
+<div>
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-primary">
+                    <i class="far fa-user"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Karya anak didik</h4>
+                    </div>
+                    <div class="card-body">
+                        0
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-danger">
+                    <i class="far fa-newspaper"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Total Murid</h4>
+                    </div>
+                    <div class="card-body">
+                        {{ $data['student'] }}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-warning">
+                    <i class="far fa-file"></i>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Total Imaji Academy</h4>
+                    </div>
+                    <div class="card-body">
+                        {{$data['imajiAcademy']}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        @foreach(\App\Models\ImajiAcademy::get() as $im)
+            @isset($activity[$im->id])
+            <div class="col-lg-6 col-md-12 col-sm-6 col-12">
+                <div>
+                    <h1 class="text-primary text-title font-weight-bold " style="font-size: 20px">Aktifitas {{$im->title}}</h1>
+                    <canvas id="activity{{$im->id}}" height="150"></canvas>
+                    <script>
+                        window.addEventListener('DOMContentLoaded', function () {
+                            function shuffle(array) {
+                                var currentIndex = array.length, randomIndex;
+                                while (currentIndex != 0) {
+                                    randomIndex = Math.floor(Math.random() * currentIndex);
+                                    currentIndex--;
+                                    [array[currentIndex], array[randomIndex]] = [
+                                        array[randomIndex], array[currentIndex]];
+                                }
+                                return array;
+                            }
+
+                            var statistics_chart = document.getElementById("activity{{$im->id}}").getContext('2d');
+                            @foreach($activity[$im->id] as $key=>$val)
+                            var data{{$key}} = [
+                                @foreach($val as $a)
+                                    {{$a}},
+                                @endforeach
+                            ];
+                            @endforeach
+                            var borderColor = ['#FAA255', '#F0C348', '#E27CF1', '#F562AC', '#EB5959', '#9EE67A', '#50D989', '#66CFF2', '#7F7CE6'];
+                            borderColor = shuffle(borderColor);
+                            new Chart(statistics_chart, {
+                                type: 'bar',
+                                data: {
+                                    labels: [
+                                        @for($i=1;$i<=$max[$im->id];$i++)
+                                            'Pertemuan {{$i}}',
+                                        @endfor
+                                    ],
+                                    datasets: [
+                                            @php($l=0)
+                                            @foreach($activity[$im->id] as $key=>$val)
+                                        {
+                                            @php($l+=1)
+                                            label: '{{$key}}',
+                                            data: data{{$key}},
+                                            borderWidth: 5,
+                                            borderColor: borderColor[{{$l}}],
+                                            backgroundColor: borderColor[{{$l}}] + '44',
+                                            pointBackgroundColor: '#fff',
+                                            pointBorderColor: borderColor[{{$l}}],
+                                            pointRadius: 3
+                                        },
+                                        @endforeach
+                                    ]
+                                },
+                                options: {
+                                    scales: {
+                                        yAxes: [{
+                                            ticks: {
+                                                beginAtZero: true
+                                            }
+                                        }]
+                                    }
+                                }
+                            });
+                        });
+                    </script>
+                </div>
+            </div>
+            @endisset
+        @endforeach
+        <div class="col-lg-6 col-md-6 col-sm-6 col-12">
+
+        </div>
+    </div>
+</div>
